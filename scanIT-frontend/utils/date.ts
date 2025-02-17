b67fdc286817e8f5b52ceedca711f99a9b9e29ca
MrV09 <vladcirlugea@gmail.com>
2025-02-17 15:00:31 +0200

    HotFix for auth(Added birthday for register)

---
+++ scanIT-frontend/utils/date.ts
@@ -0,0 +1,10 @@
+export const formatDateToString = (date: Date | null) => {
+    if(!date){
+        return '';
+    }
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+
+    return `${day}-${month}-${year}`;
+}
